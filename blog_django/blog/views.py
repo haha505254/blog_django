e3ad15ee23e35921b8b2a0d1baa11869e64c250f
haha505254 <haha505254@gmail.com>
2022-07-20 17:17:23 +0800

    add post urls

--- blog_django/blog/views.py
+++ blog_django/blog/views.py
@@ -6,3 +6,13 @@ from .models import Post
 def post_list(request):
     posts =  Post.objects.all()
     return render(request,'blog/post/list.html',locals())
+
+
+def post_detail(request,year,month,day,slug):
+    post =  Post.objects.filter(
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        slug=slug
+    ).first()
+    return render(request,'blog/post/detail.html',locals())
