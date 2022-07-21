04e29e7ca3ad2ce5ed937d52d26d8455e64cfcad
haha505254 <haha505254@gmail.com>
2022-07-21 20:41:57 +0800

    show comments

--- blog_django/blog/views.py
+++ blog_django/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-
+from .forms import CommentForm
 # Create your views here.
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     posts_list =  Post.objects.all()
-    paginator = Paginator(posts_list, 1)
+    paginator = Paginator(posts_list, 3)
     page = request.GET.get('page')  # 获取页码
     try:
         posts = paginator.page(page)  # 获取某页对应的记录
@@ -24,4 +24,20 @@ def post_detail(request,year,month,day,slug):
         publish__day=day,
         slug=slug
     ).first()
+
+        # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,'blog/post/detail.html',locals())
